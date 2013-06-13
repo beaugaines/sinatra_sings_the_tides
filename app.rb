31de283100b372c6c6cd9170c5d7f63654b136b5
Charlie Gaines <beaugaines@gmail.com>
2013-06-12 23:20:53 -0400

    sin contrib

--- app.rb
+++ app.rb
@@ -6,6 +6,9 @@ require 'haml'
 require 'susy'
 require 'fuzzy_time'
 
+# access Wundround key
+wunderground_key = ENV['WUNDERGROUND_KEY']
+
 # helpers
 require './lib/render_partial'
 
@@ -16,11 +19,27 @@ set :public_dir, 'public'
 
 
 configure do
-  # set :haml, { :format => :html5, :escape_html => true }
+  set :haml, { :format => :html5 }
   set :sass, { :style => :compact, :debug_info => false }
   Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.rb'))
 end
 
+before do
+    # new wunderground object
+    w_api = Wunderground.new("7d43f996448b0cfa")
+    # get request ip - hardcoded for now
+    # loc = Geocoder.search(request.ip)
+    @loc = Geocoder.search("64.148.1.83")[0].latitude
+    # parse lat and long
+    # lat, long = @loc[0].latitude, @loc[0].longitude
+    # retrieve tides object
+    # tides = w_api.tide_for(lat, long)
+    # get low tide
+    # @low_tide_time = tides['tide']['tideSummary']['date']['pretty']
+
+end
+  # before {@loc = request.location.city}
+  before { @title = 'Hey there!' }
 
 # routes
 get '/' do
