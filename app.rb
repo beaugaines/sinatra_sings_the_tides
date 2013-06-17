f2a929392bda622355e993c93ccfa44da5145b05
Charlie Gaines <beaugaines@gmail.com>
2013-06-17 16:24:45 -0400

    ajaxifiing form submit

--- app.rb
+++ app.rb
@@ -1,12 +1,9 @@
 require 'compass'
 require 'sinatra'
-require 'sinatra/reloader'
 require 'sinatra/static_assets'
 require 'wunderground'
-require 'geocoder'
 require 'haml'
 require 'susy'
-require 'fuzzy_time'
 
 
 # monkey patch Time
@@ -49,18 +46,18 @@ before do
     @w_api ||= Wunderground.new("7d43f996448b0cfa")
     # get request ip - hardcoded for now
     # loc = Geocoder.search(request.ip)
-    @loc = Geocoder.search("64.148.1.83")
+    # @loc = Geocoder.search("64.148.1.83")
     # parse lat and long
-    lat, long = @loc[0].latitude, @loc[0].longitude
-    # retrieve tides object
-    tides = @w_api.tide_for("#{lat},#{long}")
+    # # lat, long = @loc[0].latitude, @loc[0].longitude
+    # # retrieve tides object
+    # tides = @w_api.tide_for("#{lat},#{long}")
     # get low tide
     # @low_tide_time = tides['tide']['tideSummary'][1]['date']['pretty'].slice(/\d+:\d+\s\w{2}/)
     # @high_tide_time = tides['tide']['tideSummary'][3]['date']['pretty'].slice(/\d+:\d+\s\w{2}/)
     # @high_tide_tomorrow = tides['tide']['tideSummary'][7]['date']['pretty'].slice(/\d+:\d+\s\w{2}/)
-    @low_tide_time = Time.at(tides['tide']['tideSummary'][0]['date']['epoch'].to_i).fuzzy
-    @low_tide_time = Time.at(tides['tide']['tideSummary'][3]['date']['epoch'].to_i).fuzzy
-    @high_tide_tomorrow = Time.at(tides['tide']['tideSummary'][7]['date']['epoch'].to_i).fuzzy
+    # @low_tide_time = Time.at(tides['tide']['tideSummary'][0]['date']['epoch'].to_i).fuzzy
+    # @low_tide_time = Time.at(tides['tide']['tideSummary'][3]['date']['epoch'].to_i).fuzzy
+    # @high_tide_tomorrow = Time.at(tides['tide']['tideSummary'][7]['date']['epoch'].to_i).fuzzy
 
 end
 
@@ -74,25 +71,29 @@ get '/about' do
 end
 
 
-post '/tides' do
-  if params([:zip]).exists?
-    zip = params([:zip])
-  else
-    city = params([:city])
-    state = params([:state])
-  end
+post '/' do
+  puts params.inspect
+  upcoming_tides = []
+  city = params[:city]
+  state = params[:state]
   tides = @w_api.tide_for(state, city)
-
-  time, meridian = tides['tide']['tideSummary'][1]['date']['pretty'].slice(/\d+:\d+\s\w{2}/).split
-  if meridian == 'PM':
-    @low_tide_time = "#{time} in the PM"
-  else
-    @low_tide_time = "#{time} in the AM"
+  next_highs = tides['tide']['tideSummary'].select { |t| t['data']['type'] == 'High Tide' }[0..2]
+  next_highs.each_with_index do |item|
+    time, meridian =  item['date']['pretty'].slice(/\d+:\d+\s\w{2}/).split
+    if meridian == 'PM'
+      upcoming_tides << "#{time} in the PM"
+    else
+      upcoming_tides << "#{time} in the AM"
+    end
   end
-  
-  time, meridian = tides['tide']['tideSummary'][3]['date']['pretty'].slice(/\d+:\d+\s\w{2}/)
-  @high_tide_tomorrow = tides['tide']['tideSummary'][7]['date']['pretty'].slice(/\d+:\d+\s\w{2}/)
+  @tide1 = upcoming_tides[0]
+  @tide2 = upcoming_tides[1]
+  @tide3 = upcoming_tides[2]
 end
+  
+  # time, meridian = tides['tide']['tideSummary'][3]['date']['pretty'].slice(/\d+:\d+\s\w{2}/)
+  # @high_tide_tomorrow = tides['tide']['tideSummary'][7]['date']['pretty'].slice(/\d+:\d+\s\w{2}/)
+
 
 # get '/stylesheets/:name.css' do
 #   content_type 'text/css', :charset => 'utf-8'
--- views/application.coffee
+++ views/application.coffee
@@ -5,20 +5,38 @@
 #   else
 #     body.hide()
 
+# ajax form submit
+
+
+$ ->
+  $("#search").submit (e) ->
+    e.preventDefault()
+    $.ajax
+      type: "POST"
+      url: "/"
+      data: @serialize()
+      success: ->
+        $('#search').hide()
+        $('#results').show()
+        $('#results').html('Next high tide at ' + @tide_1)
+      error: ->
+        $('#results').html('No results for that location')
+
+
+
 # geolocate fcn
 $ ->
   getLocation = ->
     if Modernizr.geolocation
       timeoutVal = 10 * 1000 * 1000
       navigator.geolocation.getCurrentPosition
-        showPosition,
-        displayError
-        { enableHighAccuracy: true, timeout: timeoutVal,
-          maximumAge: 0 }
+      showPosition
+      displayError
+      { enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0 }
     else
       alert("Geolocation not supported on your browser")
   showPosition = (position) ->
-    $('#search').text("Lat:" + position.coords.latitude + "<br>Long:" + position.coords.longitude)
+    $('#geoloc').text("Lat:" + position.coords.latitude + "<br>Long:" + position.coords.longitude)
   displayError = (error)->
     errors = {
       1: 'Permission denied',
