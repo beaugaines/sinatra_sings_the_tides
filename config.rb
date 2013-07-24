f76cb1e3086184a3283107a7174ce0b774548155
Charlie Gaines <beaugaines@gmail.com>
2013-07-24 11:19:02 -0400

    uncommented http path config

--- config.rb
+++ config.rb
@@ -4,18 +4,18 @@ require './app'
 # Configuration to use when running within Sinatra
 # project_path          = Sinatra::Application.root
 
-# # HTTP paths
-# http_path             = '/'
-# http_stylesheets_path = '/stylesheets'
-# http_images_path      = '/images'
-# http_javascripts_path = '/javascripts'
+# HTTP paths
+http_path             = '/'
+http_stylesheets_path = '/stylesheets'
+http_images_path      = '/images'
+http_javascripts_path = '/javascripts'
 
-# # File system locations
-# css_dir               = File.join 'public', 'stylesheets'
-# sass_dir              = File.join 'views', 'stylesheets'
-# images_dir            = File.join 'public', 'images'
-# javascripts_dir       = File.join 'public', 'javascripts'
+# File system locations
+css_dir               = File.join 'public', 'stylesheets'
+sass_dir              = File.join 'views', 'stylesheets'
+images_dir            = File.join 'public', 'images'
+javascripts_dir       = File.join 'public', 'javascripts'
 
-# # Syntax preference
-# preferred_syntax      = :sass
+# Syntax preference
+preferred_syntax      = :sass
 
